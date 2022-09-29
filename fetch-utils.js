09eed77fd46dcea44043d2b6bdf53de9be639802
Emily Sellers <emilymelindasellers@gmail.com>
2022-09-28 17:01:33 -0700

    add html and supabase url and key

--- fetch-utils.js
+++ fetch-utils.js
@@ -1,5 +1,6 @@
-const SUPABASE_URL = '';
-const SUPABASE_KEY = '';
+const SUPABASE_URL = 'https://taouldlovpxzbgbxeshh.supabase.co';
+const SUPABASE_KEY =
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRhb3VsZGxvdnB4emJnYnhlc2hoIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjQyOTQ4OTUsImV4cCI6MTk3OTg3MDg5NX0.DcIS2QzuJnAiXH5DuA_OMfPoeOZ4--TU9H5lqdCZKUQ';
 const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
 /* Auth related functions */
